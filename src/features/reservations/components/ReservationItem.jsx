5ba99ebb9f3651690bc787fe9770b1f387fed263
yejiniee <02doyoung01@naver.com>
2025-05-28 16:21:57 +0900

    ♻️ refactor: props 이름 변경 (item- >reservation) 및 아이템 클릭 시 모달 열기 기능 추가

--- src/features/reservations/components/ReservationItem.jsx
+++ src/features/reservations/components/ReservationItem.jsx
@@ -1,41 +1,51 @@
-export default function ReservationItem({ item }) {
-  // console.log("item", item);
+import useModalStore from "@/stores/useModalStore";
+
+export default function ReservationItem({ reservation }) {
+  const { openModal } = useModalStore();
+
   return (
-    <div className="mb-5 flex w-full flex-col items-center">
-      <div className="flex flex-col items-center gap-3 self-stretch">
+    <div
+      onClick={() => openModal("reservation", reservation)}
+      className="flex w-full cursor-pointer flex-col items-center hover:bg-gray-50"
+    >
+      <div className="flex flex-col items-center gap-3 self-stretch pt-3">
         <div className="flex w-full items-center gap-2 text-sm font-normal text-gray-800">
           {/* 예약자명 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.name}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {reservation.name}
           </div>
           {/* 이벤트 날짜 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.event_date}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {reservation.event_date}
           </div>
           {/* 서비스 시간 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.service_time}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {reservation.service_time}
           </div>
           {/* 서비스 장소 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.location}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {reservation.location}
           </div>
           {/* 번호 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.whatsapp}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {reservation.whatsapp}
           </div>
           {/* 예약 상태- 확약/미확약 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.status}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {reservation.status}
           </div>
           {/* 폼 생성 시간 */}
-          <div className="flex shrink-0 grow basis-0 items-start self-stretch">
-            {item.created_at}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {/* // TODO: 유틸 함수로 빼기 */}
+            {new Date(reservation.created_at).toLocaleString("ko-KR", {
+              timeZone: "Asia/Seoul",
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
           </div>
-          {/* //TODO: 햄버거 아이콘 클릭 시 더보기/수정 가능 */}
-          <button className="flex shrink-0 grow basis-0 items-start">
-            ...
-          </button>
         </div>
         <div className="h-[0.0625rem] w-full bg-gray-100"></div>
       </div>
