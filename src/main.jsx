fd9416d7531796f0cd0815354e2a462bc0a14db4
yejiniee <02doyoung01@naver.com>
2025-05-28 16:46:53 +0900

    🌱 chore: React Query 초기 설정 및 QueryClientProvider 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
