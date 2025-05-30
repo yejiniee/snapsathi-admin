6cc147249ec571e7f7b4a712c2957da59d5acd9d
yejiniee <02doyoung01@naver.com>
2025-05-30 16:57:28 +0900

    ✨ feat: 예약 목록 조회 커스텀 훅에 페이지네이션 추가

--- src/features/reservations/hooks/useGetReservations.js
+++ src/features/reservations/hooks/useGetReservations.js
@@ -4,10 +4,10 @@ import getReservations from "../api/getReservations";
 /**
  * @param {"confirmed" | "unconfirmed" | undefined} selectedTabLabel
  */
-export default function useGetReservations(selectedTabLabel) {
+export default function useGetReservations({ status, page = 1, limit }) {
   return useQuery({
-    queryKey: ["reservations", selectedTabLabel],
-    queryFn: () => getReservations(selectedTabLabel),
+    queryKey: ["reservations", status, page],
+    queryFn: () => getReservations({ status, page, limit }),
     keepPreviousData: true,
     staleTime: 1000 * 30,
   });
