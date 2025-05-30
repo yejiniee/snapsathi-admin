155b44f26625b09da11aba7e88f9d09b73c8a9c2
yejiniee <02doyoung01@naver.com>
2025-05-30 13:38:03 +0900

    ✨ feat: 로그인페이지 리다이렉션 적용

--- src/features/auth/pages/SignInPage.jsx
+++ src/features/auth/pages/SignInPage.jsx
@@ -1,6 +1,9 @@
+import useRedirectIfAuthenticated from "@hooks/useRedirectIfAuthenticated";
 import SignInForm from "../components/SignInForm";
 
 export default function SignInPage() {
+  useRedirectIfAuthenticated();
+
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-4 bg-gray-50">
       <h1 className="text-xl font-bold">SNAPSATHI 예약 관리 시스템</h1>
