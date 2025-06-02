70fe1f11fdb3aa5ba86e52983620ab8011b4415a
yejiniee <02doyoung01@naver.com>
2025-06-02 16:52:43 +0900

    ♻️ refactor: 사이드바 메뉴 리스트 변경 및 링크 연결

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Sidebar() {
+  const navigate = useNavigate();
+
   return (
     <aside className="flex h-screen flex-col items-start gap-8 bg-[#0E1B6B]">
       <header className="flex w-full shrink-0 items-center justify-center p-2 text-white">
         <img src="./src/assets/logo.png"></img>
       </header>
-      {/* //TODO 사이트 목록 */}
       <nav className="flex flex-col items-start justify-center gap-6 p-5 text-white">
-        <button>SNAPSATHI</button>
-        <button>다른 사이트</button>
+        <button onClick={() => navigate("/reservation")}>예약관리</button>
       </nav>
     </aside>
   );
