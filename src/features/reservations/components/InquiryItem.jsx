d37a01c09c938e0e52d294a6fc66146c07dd2a88
yejiniee <02doyoung01@naver.com>
2025-06-09 17:23:07 +0900

    ✨ feat: 고객 문의 확인 가능한InquiryItem과 InquiryList 컴포넌트 생성

---
+++ src/features/reservations/components/InquiryItem.jsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
+export default function InquiryItem({ inquiry }) {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate(`/reservation-inquiry/${inquiry.id}`)}
+      className="flex w-full cursor-pointer flex-col items-center hover:bg-gray-50"
+    >
+      <div className="flex flex-col items-center gap-3 self-stretch pt-3">
+        <div className="flex w-full items-center gap-2 text-sm font-normal text-gray-800">
+          {/* 예약 번호 */}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {inquiry.reservation_number}
+          </div>
+          {/* //TODO 메시지 컬럼 추가할듯 */}
+          {/* 폼 생성 시간 */}
+          <div className="flex shrink-0 grow basis-0 items-center self-stretch">
+            {/* // TODO: 유틸 함수로 빼기 */}
+            {new Date(inquiry.created_at).toLocaleString("ko-KR", {
+              timeZone: "Asia/Seoul",
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </div>
+        </div>
+        <div className="h-[0.0625rem] w-full bg-gray-100"></div>
+      </div>
+    </div>
+  );
+}
