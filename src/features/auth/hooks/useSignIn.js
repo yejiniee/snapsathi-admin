7338a3306b8120424f594ceb2cf772673b764305
yejiniee <02doyoung01@naver.com>
2025-06-11 17:33:50 +0900

    ✨ feat: 로그인 시 유저 정보 스토리지에 저장

--- src/features/auth/hooks/useSignIn.js
+++ src/features/auth/hooks/useSignIn.js
@@ -1,13 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import useUserStore from "../../../stores/useUserStore";
 import { signIn } from "../api/auth";
 
 export const useSignIn = () => {
   const navigate = useNavigate();
+  const { setUser } = useUserStore();
 
   return useMutation({
     mutationFn: signIn,
-    onSuccess: () => {
+    onSuccess: (data) => {
+      setUser(data.user);
       navigate("/reservation", { replace: true });
     },
     onError: (error) => {
