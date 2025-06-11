3257d39ebb6b8764a45fbc4329780f39add31cc1
yejiniee <02doyoung01@naver.com>
2025-06-11 17:34:12 +0900

    ✨ feat: 로그아웃 시 유저 정보 스토리지에서 삭제

--- src/features/auth/hooks/useSignOut.js
+++ src/features/auth/hooks/useSignOut.js
@@ -1,13 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import useUserStore from "../../../stores/useUserStore";
 import { signOut } from "../api/auth";
 
 export const useSignOut = () => {
   const navigate = useNavigate();
+  const { clearUser } = useUserStore();
 
   return useMutation({
     mutationFn: signOut,
     onSuccess: () => {
+      clearUser();
       navigate("/signin", { replace: true });
     },
     onError: (error) => {
