fdb2c276808cb78c0342d92b4bd4c4d94795cbe3
yejiniee <02doyoung01@naver.com>
2025-06-13 13:28:56 +0900

    ♻️ refactor: 각 페이지에 공통 레이아웃 적용

--- src/features/reservations/pages/ReservationInquiryPage.jsx
+++ src/features/reservations/pages/ReservationInquiryPage.jsx
@@ -1,5 +1,3 @@
-import Header from "@components/Header";
-import Sidebar from "@components/Sidebar";
 import useRedirectIfUnauthenticated from "@hooks/useRedirectIfUnauthenticated";
 import InquiryContent from "../components/InquiryContent";
 
@@ -7,19 +5,10 @@ export default function ReservationInquiryPage() {
   useRedirectIfUnauthenticated();
 
   return (
-    <div className="flex h-full flex-1 flex-row bg-gray-50">
-      <Sidebar />
+    <main className="flex flex-col gap-5 px-10 py-5">
+      <h1 className="text-[2rem] font-semibold text-black">고객 문의 관리</h1>
 
-      <div className="flex min-h-screen w-full flex-col">
-        <Header title="고객 문의 관리" />
-        <main className="flex flex-col gap-5 px-10 py-5">
-          <h1 className="text-[2rem] font-semibold text-black">
-            고객 문의 관리
-          </h1>
-
-          <InquiryContent />
-        </main>
-      </div>
-    </div>
+      <InquiryContent />
+    </main>
   );
 }
--- src/features/reservations/pages/ReservationPage.jsx
+++ src/features/reservations/pages/ReservationPage.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import Header from "../../../components/Header";
-import Sidebar from "../../../components/Sidebar";
 import TabMenu from "../../../components/Tab/TabMenu";
 import useRedirectIfUnauthenticated from "../../../hooks/useRedirectIfUnauthenticated";
 import ReservationContent from "../components/ReservationContent";
@@ -12,25 +10,18 @@ export default function ReservationPage() {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
-    <div className="flex h-full flex-1 flex-row bg-gray-50">
-      <Sidebar />
+    <main className="flex flex-col gap-5 px-10 py-5">
+      <h1 className="text-[2rem] font-semibold text-black">예약 관리</h1>
+      <TabMenu
+        tabs={TABS}
+        selectedIndex={selectedIndex}
+        onSelect={setSelectedIndex}
+      />
 
-      <div className="flex min-h-screen w-full flex-col">
-        <Header title="예약 관리" />
-        <main className="flex flex-col gap-5 px-10 py-5">
-          <h1 className="text-[2rem] font-semibold text-black">예약 관리</h1>
-          <TabMenu
-            tabs={TABS}
-            selectedIndex={selectedIndex}
-            onSelect={setSelectedIndex}
-          />
-
-          <ReservationContent
-            selectedTabTitle={TABS[selectedIndex].title}
-            selectedTabLabel={TABS[selectedIndex].label}
-          />
-        </main>
-      </div>
-    </div>
+      <ReservationContent
+        selectedTabTitle={TABS[selectedIndex].title}
+        selectedTabLabel={TABS[selectedIndex].label}
+      />
+    </main>
   );
 }
