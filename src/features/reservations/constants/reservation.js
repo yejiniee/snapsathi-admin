8ea4aa0838bffb5ac20214cb1f917c9c4267f907
yejiniee <02doyoung01@naver.com>
2025-06-13 13:27:12 +0900

    ♻️ refactor: 예약신청폼 기본값 상수 추가

--- src/features/reservations/constants/reservation.js
+++ src/features/reservations/constants/reservation.js
@@ -5,3 +5,27 @@ export const TABS = [
 ];
 
 export const PAGE_LIMIT = 10;
+
+export const DEFAULT_FORM_VALUES = {
+  reservation_number: 0,
+  status: "",
+  name: "",
+  whatsapp: "",
+  event_date: "",
+  location: "",
+  service_hours: "",
+  theme: "",
+  event_name: "",
+  photo_style: "",
+  color_presets: [],
+  custom_color: "",
+  design_requests: "",
+  deposit_name: "",
+  discount_code: "",
+  applied_discount: "",
+  deposit_amount: "",
+  total_amount: "",
+  created_at: "",
+  modified_at: "",
+  modified_by: "",
+};
